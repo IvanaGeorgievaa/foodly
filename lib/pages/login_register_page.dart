f9640862f092ec283bbce0d8290e78197450302e
Ivana Georgieva <ivanag@vestelglobal.com>
2023-01-30 19:11:02 +0100

    Login and Register changed

--- lib/pages/login_register_page.dart
+++ lib/pages/login_register_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../Authentication/auth.dart';
 
 class LoginPage extends StatefulWidget {
@@ -43,19 +44,65 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _title() {
-    return const Center(child: Text('Foodly Auth'));
+    return const Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: Center(child: Text('Foodly Auth')),
+    );
+  }
+
+  Widget _container() {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+        height: size.height * 0.4 - 80,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(90),
+              bottomRight: Radius.circular(20)),
+          color: Color.fromARGB(255, 243, 197, 47),
+        ),
+        child: const Center(
+          // ignore: unnecessary_const
+          child: Text(
+            'FOODLY',
+            style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'RobotoMono'),
+          ),
+        ));
   }
 
   Widget _titleText() {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Text(isLogin ? 'Login' : 'Register',
-          style: const TextStyle(
-              fontSize: 28,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1,
-              fontFamily: 'RobotoMono')),
+    return Container(
+      alignment: Alignment.topLeft,
+      child: const Padding(
+        padding: EdgeInsets.only(top: 40.0, left: 30.0),
+        child: Text("Hello",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                fontSize: 34,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+                fontFamily: 'RobotoMono')),
+      ),
+    );
+  }
+
+  Widget _underText() {
+    return Container(
+      alignment: Alignment.topLeft,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0, left: 30.0, bottom: 30.0),
+        child: Text(
+            isLogin ? 'Sign into your account' : 'Create your account below!',
+            style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+                fontWeight: FontWeight.normal,
+                letterSpacing: 1,
+                fontFamily: 'RobotoMono')),
+      ),
     );
   }
 
@@ -63,10 +110,13 @@ class _LoginPageState extends State<LoginPage> {
     String title,
     TextEditingController controller,
   ) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: title,
+    return Padding(
+      padding: const EdgeInsets.only(right: 30, left: 30),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: title,
+        ),
       ),
     );
   }
@@ -75,11 +125,14 @@ class _LoginPageState extends State<LoginPage> {
     String title,
     TextEditingController controller,
   ) {
-    return TextField(
-      obscureText: true,
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: title,
+    return Padding(
+      padding: const EdgeInsets.only(right: 30, left: 30),
+      child: TextField(
+        obscureText: true,
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: title,
+        ),
       ),
     );
   }
@@ -89,18 +142,23 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _submitButton() {
-    return SizedBox(
-      width: 200,
-      child: ElevatedButton(
-          onPressed: isLogin
-              ? signInWithEmailAndPassword
-              : createUserWithEmailAndPassword,
-          style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ))),
-          child: Text(isLogin ? 'Login' : 'Register')),
+    return Padding(
+      padding: const EdgeInsets.only(top: 40.0),
+      child: SizedBox(
+        width: 200,
+        child: ElevatedButton(
+            onPressed: isLogin
+                ? signInWithEmailAndPassword
+                : createUserWithEmailAndPassword,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    const Color.fromARGB(255, 243, 197, 47)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ))),
+            child: Text(isLogin ? 'Login' : 'Register')),
+      ),
     );
   }
 
@@ -111,31 +169,30 @@ class _LoginPageState extends State<LoginPage> {
           isLogin = !isLogin;
         });
       },
-      child: Text(isLogin
-          ? 'Not registered yet? Create an account'
-          : 'Already registered? Log in'),
+      child: Text(
+        isLogin
+            ? 'Not registered yet? Create an account'
+            : 'Already registered? Log in',
+        style: const TextStyle(color: Color.fromARGB(255, 243, 197, 47)),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _titleText(),
-            _entryFieldEmail('Email', _controllerEmail),
-            _entryFieldPassword('Password', _controllerPassword),
-            _errorMessage(),
-            _submitButton(),
-            _loginOrRegisterButton(),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          _container(),
+          _titleText(),
+          _underText(),
+          _entryFieldEmail('Email', _controllerEmail),
+          _entryFieldPassword('Password', _controllerPassword),
+          _errorMessage(),
+          _submitButton(),
+          _loginOrRegisterButton(),
+        ],
       ),
     );
   }
