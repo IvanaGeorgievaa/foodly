82902c18fc7b0a0bdf98a8811de2dcd40b2ab37c
Ivana Georgieva <ivanag@vestelglobal.com>
2023-02-07 22:09:24 +0100

    Created the details page for each product

--- lib/widgets/single_product.dart
+++ lib/widgets/single_product.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../custom_text.dart';
 import '../models/product.dart';
+import '../pages/details_page.dart';
 
 class SingleProductWidget extends StatelessWidget {
   final ProductModel product;
@@ -13,87 +14,94 @@ class SingleProductWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10.0),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey.withOpacity(.5),
-                  offset: const Offset(3, 2),
-                  blurRadius: 7)
-            ]),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 30.0, top: 20.0),
-                child: Text(
-                  product.name,
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 30.0),
-                child: Text(
-                  "category: " "${product.category}",
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 243, 197, 47),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                    ),
-                    onPressed: () => Null,
-                    child: CustomText(
-                      text: "${product.price} ден",
-                      size: 16,
-                      weight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+      child: InkWell(
+        onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DetailsScreen(product: product))),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(.5),
+                    offset: const Offset(3, 2),
+                    blurRadius: 7)
+              ]),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30.0, top: 20.0),
+                  child: Text(
+                    product.name,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
                 ),
-                IconButton(
-                    icon: const Icon(Icons.add_shopping_cart),
-                    onPressed: () {}),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 10, bottom: 20, right: 10),
-                  child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                      child: Image.network(product.image, width: 120)),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Text(
+                    "category: " "${product.category}",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey),
+                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 243, 197, 47),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                      ),
+                      onPressed: () => Null,
+                      child: CustomText(
+                        text: "${product.price} ден",
+                        size: 16,
+                        weight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                      icon: const Icon(Icons.add_shopping_cart),
+                      onPressed: () {}),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, bottom: 20, right: 10),
+                    child: ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                        child: Image.network(product.image, width: 120)),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
